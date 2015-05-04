b6180ff6408aa18f4dd97f17ecb1aaa189e5bde6
Arnaud Méhat <amehat@cultivonsleweb.com>
2015-05-04 11:11:19 +0200

    Gestion du fait qu'il ne puisse pas y avoir de valeur par defaut dans le slug

--- components/SlugUrlManager.php
+++ components/SlugUrlManager.php
@@ -11,19 +11,58 @@ class SlugUrlManager extends UrlManager {
     public function init (){
         foreach (ClwSlugs::find()->all() as $obj){
 
-            $this->addRules([
-                [
+            foreach (ClwSlugs::find()->all() as $obj){
+                $rules = [
                     'pattern' => "$obj->pattern",
                     'route' => "$obj->route",
-                    'suffix' => "$obj->suffix",
-                    'defaults' => "$obj->defaults"
-                ]
-            ], true);
+                    'suffix' => "$obj->suffix"
+                ];
+                $default = $obj->defaults;
+                if (  0 != strlen((string)$default) ){
+                    $rules['defaults'] = (array)"$obj->defaults";
+                }
+
+                $this->addRules([
+                    $rules
+                ], true);
+            }
         }
 
         parent::init();
     }
 
+    public function parseRequest($request){
+        return ['site/test'];
+        if ($this->enablePrettyUrl) {
+            $pathInfo = $request->getPathInfo();
+            if ( strstr($pathInfo, 'slug/')){
+                $str = explode("/",$pathInfo);
+                $key = 0;
+                foreach ($str as $index => $tag){
+                    if ( $tag == 'slug' ){
+                        $key = $index+1;
+                    }
+                }
+                if ( 0 != $key ){
+                    return ClwSlugs::find()->where([
+                        'id_slug' => $key
+                    ])->One()->route;
+                }
+            }
+            /*
+            $str = explode("/",$pathInfo);
+            print_r ( $str );
+            foreach (ClwSlugs::find()->where([
+                'id_slug' => ':slug'
+            ])->params([
+                    ':slug'=>$str[1]
+                ]) as $obj){
+                return $obj->route;
+            }
+            */
+        }
 
+        return parent::parseRequest($request);
+    }
 
 } 
